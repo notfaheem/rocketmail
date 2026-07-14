056b21d859ab8344d653ff4782779e196372c3fc
faheem775 <aspireaid75@gmail.com>
2026-07-14 18:37:44 +0530

    redoing the movement logic

--- game.js
+++ game.js
@@ -10,38 +10,16 @@ let rotationDirection = "";
 document.addEventListener("keydown", (e) => {
     if (gameOver) return;
 
-    const halfHeight = rocket.offsetHeight / 2;
-
     if (e.code === "ArrowUp" || e.code === "KeyW") {
-
-        
-
-        if (position - step >= halfHeight) {
-            position -= step;
-            rotationDirection = "up";
-        }
+        movement("up")
     }
 
     if (e.code === "ArrowDown" || e.code === "KeyS") {
-        if (position + step <= game.clientHeight - halfHeight) {
-            position += step;
-            rotationDirection = "down";
-        }
+        movement("down")
     }
 
-    rocket.style.top = position + "px";
-    if (rotationDirection === "up") {
-        rocket.style.rotate = "-5deg"
-        setTimeout(() => {
-            rocket.style.rotate = "0deg";
-        }, 500)
-    }else if (rotationDirection === "down"){
-        rocket.style.rotate = "20deg"
-        setTimeout(() => {
-            rocket.style.rotate = "0deg";
-        }, 500)
-    }
-});
+})
+
 
 //collision
 let gameOver = false;
