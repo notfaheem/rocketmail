3cf8b364b7acbfae71ac327baabb5dfc95ba69f4
faheem775 <aspireaid75@gmail.com>
2026-08-03 23:45:57 +0530

    background text beginning and other fixes

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 //loading 
 window.addEventListener("load", ()=>{
     const loading = document.getElementById("loading");
-    loading.style.opacity = 0
+    loading.style.opacity = 0;
+    loading.style.pointerEvents = "none";
 })
 
 //movement
